a17f6217104d789dd3c3654d2fc02d6158a16c38
Roman Kotyaev <Roman.Kotyaev@artezio.com>
2021-06-01 16:36:34 +0300

    Base vue app

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="app">
+    <div class="greeting">
+      <Test
+        msg="Create Vue App"
+      />
+      <h2 class="counter">{{ bar + baz }}</h2>
+      <input
+        type="button"
+        value="Count"
+        @click="count"
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+
+import foo from './classes/Main';
+import Test from './components/Test';
+
+export default {
+  components: { Test },
+  data() {
+    return {
+      bar: 1
+    };
+  },
+  computed: {
+    baz() {
+      return foo(3);
+    }
+  },
+  methods: {
+    count() {
+      ++this.bar;
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+  body {
+    margin: 0;
+    padding: 0;
+  }
+  .app {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    text-align: center;
+  }
+  .greeting {
+    width: 300px;
+  }
+  .counter {
+    color: cadetblue;
+  }
+</style>
---
+++ src/components/Test.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script>
+export default {
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+  h1 {
+    color: forestgreen;
+  }
+</style>
